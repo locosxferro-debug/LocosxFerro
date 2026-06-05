caf942f8f89d4905e53df2874c5652c770ae492b
locosxferro-debug <locosxferro@gmail.com>
2026-06-04 23:52:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,3 @@ const navLinks = document.getElementById("navLinks");
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
-
-navLinks.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', () => navLinks.classList.remove('active'));
-});
