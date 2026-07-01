d4cf575e43bcfe66692aad1bca53db9050cfc7a7
BrunoVerne <bverne@fi.uba.ar>
2026-07-01 14:17:45 -0300

    harcodeo porq no me toma la url front para el cors xd

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+  'http://localhost:5173',
+  'https://locosxferro-1.onrender.com',
+];
+
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      process.env.FRONTEND_LOCAL_URL, //la url del tunel del frontend
-    ],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS bloqueado para origin: ${origin}`));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
