52b32e966ea323349b8dcc63adfa57eaa9f0bf52
BrunoVerne <bverne@fi.uba.ar>
2026-07-01 14:22:26 -0300

    abro cos

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,22 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [
-  'http://localhost:5173',
-  'https://locosxferro-1.onrender.com',
-];
-
+ 
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error(`CORS bloqueado para origin: ${origin}`));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+  origin: true,
+  credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
